c490a2f2b180e4328549cfe99963ca076fa6b7a4
monaghan <monaghan@iastate.edu>
2018-12-22 00:03:20 -0600

    Adding new files to repo. Started outlining the structure of the API.

---
+++ CheckoutAPI.py
@@ -0,0 +1,42 @@
+
+
+
+class Item():
+    def __init__():
+        return
+
+    def get_price():
+        return
+
+    def update_price():
+        return
+
+
+class Checkout():
+    def __init__():
+        return
+
+    def set_price():
+        return
+
+    def change_price():
+        return
+
+    def markdown_price():
+        return
+
+    def set_special_price():
+        return
+
+    def get_items_in_cart():
+        return
+
+    def add_item_to_cart():
+        return
+
+    def remove_item_from_cart():
+        return
+
+
+class CheckoutAPI():
+    
