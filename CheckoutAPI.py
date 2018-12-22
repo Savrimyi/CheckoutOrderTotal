6b5067cb3448bef3b4f290851898a1f2fb4dd16f
monaghan <monaghan@iastate.edu>
2018-12-22 12:01:35 -0600

    type checking for Item passes

--- CheckoutAPI.py
+++ CheckoutAPI.py
@@ -3,9 +3,14 @@ import http.server
 import re
 import json
 
+valid_unit_types = ['each', 'per pound']
 
 class Item():
     def __init__(self, name, price_per_unit, unit_type):
+        #Validate input before initializing
+        if not type(name) == str: raise(ValueError)
+        if not (type(price_per_unit) == int or type(price_per_unit) == float): raise(ValueError)
+        if not unit_type in valid_unit_types: raise(ValueError)
         self.name = name
         self.price_per_unit = price_per_unit
         self.unit_type = unit_type
--- TestItem.py
+++ TestItem.py
@@ -7,8 +7,21 @@ class CheckoutAPIItemTestCase(unittest.TestCase):
     def test_init_item_with_info(self):
         try:
             item = Item("potatoes", 5, "each")
+            item = Item("potatoes", 2.45, "per pound")
         except Exception as e:
             self.fail("Raised Unexpected Exception: " + str(e))
+
+    def test_init_with_integer_for_name(self):
+        with self.assertRaises(Exception):
+            item = Item(56, 5, "each")
+
+    def test_init_with_string_for_price(self):
+        with self.assertRaises(Exception):
+            item = Item("potatoes", "five", "each")
+
+    def test_init_with_invalid_per_unit_type(self):
+        with self.assertRaises(Exception):
+            item = Item("potatoes", 5, "invalid string")
 
     def test_get_price(self):
         self.fail("Test not implemented")
