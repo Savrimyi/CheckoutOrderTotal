17c33c695137caed7f13fb5da65a40ef3f035df4
monaghan <monaghan@iastate.edu>
2018-12-24 22:35:00 -0600

    Created empty tests and functions for the PointOfSale object

---
+++ PointOfSale.py
@@ -0,0 +1,49 @@
+import re
+import requests
+
+class PointOfSale():
+    """
+        This Point of Sale object will take user input and translate it into API calls.
+    """
+    def __init__(self):
+        return
+
+    def call_api_get_item_info(self):
+        return
+
+    def call_api_get_items_in_store(self):
+        return
+
+    def call_api_get_items_in_cart(self):
+        return
+
+    def call_api_get_item_info_cart(self):
+        return
+
+    def call_api_get_checkout_total(self):
+        return
+
+    def call_api_post_add_items_to_store(self):
+        return
+
+    def call_api_post_set_special_price(self):
+        return
+
+    def call_api_post_markdown_price(self):
+        return
+
+    def call_api_post_add_item_to_cart(self):
+        return
+
+    def call_api_delete_from_store(self):
+        return
+
+    def call_api_delete_from_cart(self):
+        return
+
+    def handle_user_input(self):
+        return
+
+    def run(self):
+        while True:
+            return
---
+++ TestPointOfSale.py
@@ -0,0 +1,50 @@
+import unittest
+from PointOfSale import PointOfSale
+
+
+class PointOfSaleTestCase(unittest.TestCase):
+    """
+        Test cases for the Cashier's Point of Sale system
+    """
+
+    def setUp(self):
+        return
+
+    ### GET request (information retreival) tests
+
+    def test_valid_input_get_item_from_store(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_get_items_in_store(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_get_items_in_cart(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_get_item_info_cart(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_get_checkout_total(self):
+        self.fail("Test not implemented.")
+
+    ### POST request (Information changing) tests
+
+    def test_valid_input_add_items_to_store(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_set_special_price(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_markdown_price(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_add_item_to_cart(self):
+        self.fail("Test not implemented.")
+
+    ### DELETE request (Information removal) tests
+
+    def test_valid_input_delete_from_store(self):
+        self.fail("Test not implemented.")
+
+    def test_valid_input_delete_from_cart(self):
+        self.fail("Test not implemented.")
